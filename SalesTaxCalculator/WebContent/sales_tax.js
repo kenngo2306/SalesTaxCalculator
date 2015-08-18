910498442a88e594c8cc469e09b29ce8ffa20505
KenNgo <phucuongngo@gmail.com>
2015-08-18 09:15:51 -0400

    finish

--- SalesTaxCalculator/WebContent/sales_tax.js
+++ SalesTaxCalculator/WebContent/sales_tax.js
@@ -1,4 +1,23 @@
+
+function run()
+{
+	document.getElementById("calculate").addEventListener("click", calculateTax);
+}
 var $ = function (id) {
+	//it returns the value of the element that has the tag of id
     return document.getElementById(id); 
 }
+
+function calculateTax()
+{
+	var taxRate = parseFloat(document.getElementById("taxRate").value);
+	var subTotal = parseFloat(document.getElementById("subtotal").value);
+	
+	var tax = subTotal * taxRate / 100;
+	var total = tax + subTotal;
+	
+	document.getElementById('salesTax').value = tax;
+	document.getElementById('total').value = total;
+}
+
 
